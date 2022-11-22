3425b7015272c4d82fc346c671e5d05e67ca5f13
ps-yu <pankajsharma12540@gmail.com>
2022-11-22 18:29:04 -0500

    Find a word in the rows or column of a 2D matrix

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,23 @@
-const wordSearch = (letters, word) => { 
+const wordSearch = (letters, word) => {
+    if (word === "indefined"){
+        return false;
+    } 
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    let verticalArray = [];
+    for (let i = 0; i < letters[0].length; i++){
+        let word = "";
+        for ( let j = 0; j < letters.length; j ++){
+            word = word.concat(letters[j][i]);
+        }verticalArray.push(word);        
+    }
+    for (l of verticalArray) {
+        if (l.includes(word)) return true
+    }
+    return false;
+
 }
 
 module.exports = wordSearch
